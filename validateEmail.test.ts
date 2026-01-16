1b68a19846897bacb8f17acc574b57ddb3bf2a82
LskNoah <ciambi.bibombe@student.vinci.be>
2026-01-16 16:46:40 +0100

    add second validation email (space-between)

--- validateEmail.test.ts
+++ validateEmail.test.ts
@@ -2,8 +2,14 @@ import { Email } from "./validateEmail";
 
 describe("Email Checker",() => {
     test("Must have at least one @ ", () => {
-        const email = new Email()
-        const actual = email.emailCheck("Abcdefghij")
-        expect(actual).toBe(false)
+        const email = new Email();
+        const actual = email.emailCheck("Abcdefghij");
+        expect(actual).toBe(false);
+    })
+
+    test("Don't have a space-between",() => {
+        const email = new Email();
+        const actual = email.spaceCheck("Abcde @fghij");
+        expect(actual).toBe(false);
     })
 })
--- validateEmail.ts
+++ validateEmail.ts
@@ -8,4 +8,13 @@ export class Email {
         }
         return false;
     }
+
+    spaceCheck(email : string){
+    for(let i = 0 ; i<email.length ; i++){
+       if(email[i] === ' '){
+            return false;
+            }
+        }
+        return true;
+    }
 }
