135326ef739c2f789630970a73f63751feebb105
LskNoah <ciambi.bibombe@student.vinci.be>
2026-01-16 17:03:48 +0100

    refactor const email

--- validateEmail.test.ts
+++ validateEmail.test.ts
@@ -1,14 +1,13 @@
 import { Email } from "./validateEmail";
 
 describe("Email Checker",() => {
+    const email = new Email();
     test("Must have at least one @ ", () => {
-        const email = new Email();
         const actual = email.emailCheck("Abcdefghij");
         expect(actual).toBe(false);
     })
 
     test("Don't have a space-between",() => {
-        const email = new Email();
         const actual = email.spaceCheck("Abcde @fghij");
         expect(actual).toBe(false);
     })
